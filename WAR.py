e5410d331083cb94ebbbb104d348597f0f9ae930
jnomellini <46978925+jnomellini@users.noreply.github.com>
2021-01-10 20:28:16 -0800

    Update WAR.py

--- WAR.py
+++ WAR.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[105]:
-
-
 # CARD
 # SUIT, RANK, VALUE
 import random
@@ -12,10 +9,6 @@ suits = ('Hearts','Diamonds','Spades','Clubs')
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,
          'Queen':10, 'King':10, 'Ace':11}
-
-
-# In[106]:
-
 
 class Card:
     
@@ -27,63 +20,22 @@ class Card:
     def __str__(self):
         return self.rank + " of " + self.suit
 
-
-# In[107]:
-
-
 three_of_clubs = Card("Clubs",'Three')
-
-
-# In[108]:
-
 
 three_of_clubs.suit
 
-
-# In[109]:
-
-
-three_of_clubs.value
-
-
-# In[110]:
-
-
+three_of_clubs
 two_hearts = Card("Hearts","Two")
-
-
-# In[111]:
-
 
 print(two_hearts)
 
-
-# In[112]:
-
-
 two_hearts.suit
-
-
-# In[113]:
-
 
 two_hearts.rank
 
-
-# In[114]:
-
-
 two_hearts.value
 
-
-# In[115]:
-
-
 two_hearts.value == three_of_clubs.value
-
-
-# In[130]:
-
 
 class Deck:
     
@@ -105,63 +57,23 @@ class Deck:
     def deal_one(self):
         return self.all_cards.pop()
 
-
-# In[132]:
-
-
 new_deck = Deck()
 
-
-# In[133]:
-
-
 new_deck.shuffle()
-
-
-# In[134]:
-
 
 mycard = new_deck.deal_one()
 
-
-# In[135]:
-
-
 print(mycard)
-
-
-# In[136]:
-
 
 len(new_deck.all_cards)
 
-
-# In[118]:
-
-
 bottom_card = new_deck.all_cards[-1]
-
-
-# In[119]:
-
 
 print(bottom_card)
 
-
-# In[120]:
-
-
 new_deck.shuffle()
 
-
-# In[126]:
-
-
 print(new_deck.all_cards[0])
-
-
-# In[150]:
-
 
 class Player:
     
@@ -184,69 +96,20 @@ class Player:
     def __str__(self):
         return f'Player {self.name} has {len(self.all_cards)} cards.'
 
-
-# In[151]:
-
-
 new_player = Player("Jose")
 
-
-# In[152]:
-
-
 print(new_player)
-
-
-# In[156]:
-
 
 new_player.add_cards(mycard)
 
-
-# In[157]:
-
-
 print(new_player)
-
-
-# In[160]:
-
 
 print(new_player.all_cards[0])
 
-
-# In[161]:
-
-
 new_player.add_cards([mycard,mycard,mycard])
 
-
-# In[162]:
-
-
 print(new_player)
-
-
-# In[163]:
-
 
 new_player.remove_one()
 
-
-# In[164]:
-
-
 print(new_player)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
